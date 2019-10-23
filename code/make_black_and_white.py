be3c932105867215c8d6ffaefe5d30639345f761
eitrheim <ann.eitrheim@gmail.com>
2019-10-22 21:59:56 -0500

    Code to make it black and white with opencv

--- code/make_black_and_white.py
+++ code/make_black_and_white.py
@@ -1,11 +1,18 @@
 from PIL import Image
 import os
+# import cv2
 
 
 def black_and_white(input_image_path, output_image_path):
     color_image = Image.open(input_image_path)
     bw = color_image.convert('L')
     bw.save(output_image_path)
+
+
+# def black_and_white_opencv(input_image_path, output_image_path):
+#     image = cv2.imread(input_image_path)
+#     grayImage = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+#     cv2.imwrite(output_image_path, grayImage)
 
 
 repo_path = '/Users/anneitrheim/PycharmProjects/Colorize-Black-and-White-Photos/'
