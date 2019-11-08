626df53dde930b22670d5352dc11c34b440b8252
matthewechols <echolsmatthewg@gmail.com>
2019-11-08 14:14:25 -0600

    Added RNN

--- code/Creating_Matrix.py
+++ code/Creating_Matrix.py
@@ -1,36 +1,29 @@
 from keras.preprocessing.image import img_to_array, load_img
-import os, sys
-from PIL import Image
-from IPython.display import display
-from IPython.display import Image as _Imgdis
+import os
 import numpy as np
-
+from PIL import Image
 
 
 #Creating folder of images
-folder = "../images-colored"
-onlyfiles = [f for f in os.listdir(folder) if os.path.isfile(os.path.join(folder, f))]
+repo_path = '../images-colored/'
+file_name = [f for f in os.listdir(repo_path) if f.endswith(('.jpg', '.JPG', '.tif'))]
 
-print("Working with {0} images".format(len(onlyfiles)))
+print("Working with {0} images".format(len(file_name)))
 
 
 train_files = []
 y_train = []
-i = 0
-for _file in onlyfiles:
-    train_files.append("../Images" + _file)
+i=0
+for _file in file_name:
+    train_files.append(_file)
     label_in_file = _file.find("_")
     y_train.append(_file[0:label_in_file])
 
 print("Files in train_files: %d" % len(train_files))
 
 # Original Dimensions
-image_width = 640
-image_height = 480
-ratio = 4
-
-image_width = int(image_width / ratio)
-image_height = int(image_height / ratio)
+image_width = 200
+image_height = 200
 
 channels = 3
 nb_classes = 1
@@ -40,16 +33,19 @@ dataset = np.ndarray(shape=(len(train_files), channels, image_height, image_widt
 
 i = 0
 for _file in train_files:
-    img = load_img(folder + "/" + _file)  # this is a PIL image
-    img.thumbnail((image_width, image_height))
-    # Convert to Numpy Array
-    x = img_to_array(img)
-    x = x.reshape((3, 120, 160))
-    # Normalize
-    x = (x - 128.0) / 128.0
-    dataset[i] = x
-    i += 1
-    if i % 250 == 0:
-        print("%d images to array" % i)
+    img = load_img(repo_path + _file)  # this is a PIL image
+    try:
+        # Convert to Numpy Array
+        x = img_to_array(img)
+        x = x.reshape((3, image_width, image_height))
+        # Normalize
+        x = (x - 128.0) / 128.0
+        dataset[i] = x
+        i += 1
+        if i % 100 == 0:
+            print("%d images to array" % i)
+    except ValueError:
+        print("Value Error: ", _file)
 print("All images to array!")
+
 
